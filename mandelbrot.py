0bbad0a2f2156382cd14f422f915c0dc6e98bce1
Paola Polanco <ppolan23@student.aau.dk>
2026-02-05 15:46:57 +0100

    Add mandelbrot grid implementation

--- mandelbrot.py
+++ mandelbrot.py
@@ -16,3 +16,25 @@ def mandelbrot_point(c, max_iter=100):
     return max_iter
 
 
+def compute_mandelbrot_grid(xmin, xmax, ymin, ymax, width, height, max_iter):
+
+    # defining the region 
+    x = np.linspace(xmin, xmax, width)
+    y = np.linspace(ymin, ymax, height)
+
+    # iterations counts 2D array 
+    counts = np.zeros((height, width), dtype=int)
+
+    # loop over points 
+    for j in range(height):
+        for i in range(width):
+            c = complex(x[i], y[j])
+            counts[j, i] = mandelbrot_point(c, max_iter)
+
+    return counts
+
+if __name__ == "__main__":
+    print(mandelbrot_point(0))
+    grid = compute_mandelbrot_grid(-2, 1, -1.5, 1.5, 100, 100, 100)
+    print(grid.shape)
+

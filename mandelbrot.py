63061442700164db153c5b5eb2f494971244ed65
Paola Polanco <ppolan23@student.aau.dk>
2026-02-05 14:19:13 +0100

    Add TODO comment

---
+++ mandelbrot.py
@@ -0,0 +1,22 @@
+"""
+Mandelbrot Set Generator
+Author: Paola Polanco
+Course: Numerical Scientific Computing 2026
+"""
+
+def f(x):
+    """
+    Example function.
+
+    Parameters
+    ----------
+    x : float
+        Input value
+
+    Returns
+    -------
+    float
+        Output value
+    """
+    # TODO: Implement the algorithm
+    return x
